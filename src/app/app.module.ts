e5ab17826569dbe8519154132cd5d0ad6813a4ea
Katrin Valge <katrin.valge@mail.ee>
2023-03-26 21:00:46 +0300

    Created components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
+import { MovieListComponent } from './pages/movies/movie-list/movie-list.component';
+import { SignupComponent } from './pages/authentication/signup/signup.component';
+import { LoginComponent } from './pages/authentication/login/login.component';
+import { RentalsComponent } from './pages/rentals/rentals.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MovieListComponent,
+    SignupComponent,
+    LoginComponent,
+    RentalsComponent
   ],
   imports: [
     BrowserModule
