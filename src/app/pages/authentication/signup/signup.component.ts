2e9f9a2d3077f3f7c7fb99617d2461639c7ef460
Katrin Valge <katrin.valge@mail.ee>
2023-04-03 16:14:23 +0300

    Updated signup validation.

--- src/app/pages/authentication/signup/signup.component.ts
+++ src/app/pages/authentication/signup/signup.component.ts
@@ -20,7 +20,7 @@ export class SignupComponent implements OnInit {
     this.userForm = new FormGroup({
       name: new FormControl(null, Validators.required),
       username: new FormControl(null, Validators.required),
-      email: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, Validators.required),
     });
   }
