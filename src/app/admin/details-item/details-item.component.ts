66cf2ba9359a69662c19d7d329bc2ebafc6dad59
Katrin Valge <katrin.valge@mail.ee>
2023-04-04 17:17:03 +0300

    Created detail-item design.

--- src/app/admin/details-item/details-item.component.ts
+++ src/app/admin/details-item/details-item.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, DoCheck, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {MovieDetailsModel} from "../../pages/movies/movie-list/movie-details/movie-details.model";
+import {DataService} from "../../services/data.service";
+import {MovieService} from "../../services/movie.service";
 
 @Component({
   selector: 'app-details-item',
   templateUrl: './details-item.component.html',
   styleUrls: ['./details-item.component.css']
 })
-export class DetailsItemComponent implements OnInit {
+export class DetailsItemComponent implements OnInit, DoCheck, OnChanges {
 
-  constructor() { }
+  movieName: string = '';
+  details: MovieDetailsModel;
 
-  ngOnInit(): void {
+  constructor(private dataService: DataService, private movieService: MovieService) {
   }
 
+  ngOnInit(): void {
+    this.dataService.getDetailsByMovieName(this.movieName)
+  }
+
+  getMovie() {
+/*    this.details = this.movieService.getDetails();
+    console.log(this.details)*/
+
+    this.movieName = '';
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+
+  }
+  ngDoCheck() {
+
+  }
 }
