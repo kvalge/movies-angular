d0ec04e43cbad18ade5774fb3b9c7a6875b1c682
Katrin Valge <katrin.valge@mail.ee>
2023-04-07 14:11:49 +0300

    Added login design.

--- src/app/pages/authentication/login/login.component.ts
+++ src/app/pages/authentication/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../../../services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  userForm: FormGroup;
+  error: string = null;
 
-  ngOnInit(): void {
+  constructor(private authService: AuthService, private router: Router) {
   }
 
+  ngOnInit(): void {
+    this.userForm = new FormGroup({
+      username: new FormControl(null, Validators.required),
+      password: new FormControl(null, Validators.required),
+    });
+  }
+
+  onLogin() {
+    this.authService.login(this.userForm.value)
+      .subscribe({
+        next: (data) => console.log('data', data),
+        error: (err) => {
+          this.error = err.message;
+        },
+        complete: () => this.router.navigate(['movies'])
+      });
+    this.userForm.reset();
+  }
+
+  closeError() {
+    this.error = null;
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import {HttpClient} from "@angular/common/http";
 import {UserModel} from "../pages/authentication/user.model";
 import {Injectable} from "@angular/core";
+import {LoginModel} from "../pages/authentication/login/login.model";
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
@@ -10,5 +11,9 @@ export class AuthService {
 
   register(userModel: UserModel) {
     return this.http.post('http://localhost:8080/register', userModel)
+  }
+
+  login(loginModel: LoginModel) {
+    return this.http.post('http://localhost:8080/login', loginModel)
   }
 }
